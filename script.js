5682c9d62f11da6a5aa981f5f0557a609c87f32c
Coder2131 <cfoote2131@gmail.com>
2022-02-08 20:40:16 -0500

    1 file modified

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
 playerImage.src = "https://i.ibb.co/GFkFNyd/shadow-dog.png";
+const spriteWidth = 575;
+const spriteHeight = 523;
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.fillRect(100,50,100,100);
-  ctx.drawImage(playerImage, 0, 0);
+  //ctx.fillRect(100,50,100,100);
+  //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+  ctx.drawImage(playerImage, 0 * spriteWidth, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
   requestAnimationFrame(animate);
 }
 animate();
