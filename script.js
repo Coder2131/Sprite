da6b1188c1e9d4872f9e3ca2d1d17694bccdca71
Coder2131 <cfoote2131@gmail.com>
2022-02-08 20:57:46 -0500

    2 files modified

---
+++ script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const width = canvas.width = 320;
+const height = canvas.height = 480;
+
+canvas.style.marginTop = window.innerHeight / 2 - height / 2 + "px";
